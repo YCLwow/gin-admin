7b7575a340314b04324212747d5cbc7a9ebf4f66
liuyichen <820471261@qq.com>
2025-05-12 14:54:19 +0800

    fix:路由和数据库加载

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,3 +1,14 @@
 package config
 
+// 数据库连接配置
+const (
+	DBDriver   = "mysql"
+	DBUser     = "root"
+	DBPassword = "123456"
+	DBHost     = "localhost"
+	DBPort     = "3306"
+	DBName     = "ginadmin"
+)
+
+// JWT配置
 const JWTSecret = "your-256-bit-secret-key"
--- internal/models/user.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type User struct { // 结构体名称为 User
-	ID       int    `json:"id" db:"id"`
-	Username string `json:"username" db:"username"`
-	Email    string `json:"email" db:"email"`
-	Password string `json:"password" db:"password"`
-}
